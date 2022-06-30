ff64d5d58fa0adeb576b996bc0488b63c9a5f770
LunnaCrudi <lunnacrudi@gmail.com>
2022-06-29 21:02:41 -0300

    up

--- angular/blogLovemoon/src/app/app.module.ts
+++ angular/blogLovemoon/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 // Services
 import { BlogService } from './providers/blog.service';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { BlogService } from './providers/blog.service';
     AppComponent,
     HomePageComponent,
     PostPageComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    HeaderComponent
   ],
   imports: [
     RoutingModule,
